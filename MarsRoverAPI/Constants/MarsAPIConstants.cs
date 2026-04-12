204991fd1d83f299bca2a41b3117a155c745c01d
Andrew DeMarco <andrewd1137@gmail.com>
2026-04-12 00:37:01 -0400

    fixed issues with MarsAPIConstants where paths weren't coming out in correct format

--- MarsRoverAPI/Constants/MarsAPIConstants.cs
+++ MarsRoverAPI/Constants/MarsAPIConstants.cs
@@ -4,9 +4,9 @@ public static class MarsAPIConstants
     private const string FeedType = "feedtype=json";
     private const string Category = "category=";
     private const string Mission = "mission=";
-    public readonly static string CuriosityRoverPath = $"/rss/api/?{SetFeedAndCategory()}=msl";
-    public readonly static string PerseveranceRoverPath = $"/rss/api/?{SetFeedAndCategory()}=mars2020";
-    public readonly static string InSightLanderPath = $"/insight/api/?{SetFeedAndMission()}=insight";
-    private static string SetFeedAndCategory() => $"?{Feed}&{FeedType}&{Category}=";
-    private static string SetFeedAndMission() => $"?{Feed}&{FeedType}&{Mission}=";
+    public readonly static string CuriosityRoverPath = $"/rss/api/{SetFeedAndCategory("msl")}";
+    public readonly static string PerseveranceRoverPath = $"/rss/api/{SetFeedAndCategory("mars2020")}";
+    public readonly static string InSightLanderPath = $"/insight/api/?{SetFeedAndMission("insight")}";
+    private static string SetFeedAndCategory(string category) => $"?{Feed}&{FeedType}&{Category}{category}";
+    private static string SetFeedAndMission(string mission) => $"?{Feed}&{FeedType}&{Mission}{mission}";
 }
